b5b88848ea52de27d084bac634420db3980520da
tj <tjyoon0324@gmail.com>
2022-07-14 20:19:16 +0900

    Adding a new practice

--- pracitces/Sum of two lowest postiive integers.py
+++ pracitces/Sum of two lowest postiive integers.py
@@ -1,6 +1,13 @@
+# At first, I tried below
+
 def sum_two_smallest_numbers(numbers):
     low = min(numbers)
     numbers.pop(numbers.index(low))
     second_low = min(numbers)
     return low + second_low
 
+# However, below is much better
+
+
+def sum_two_smallest_numbers(numbers):
+    return sum(sorted(numbers)[:2])
